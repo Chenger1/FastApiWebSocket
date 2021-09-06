c8e343a33bfa142f73b9c9a4c0aad00493569890
Chenger1 <exs2199@gmail.com>
2021-09-06 22:41:55 +0300

    Add websocket ConnectionManager

---
+++ app/connection_manager.py
@@ -0,0 +1,23 @@
+from fastapi import WebSocket
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connection: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connection.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connection.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connection:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
