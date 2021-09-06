fda8bc04d14fb25dfe69f81280f5e6bc12c65d59
Chenger1 <exs2199@gmail.com>
2021-09-07 00:05:35 +0300

    Authentication dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -6,5 +6,8 @@ from typing import Optional
 async def get_user(websocket: WebSocket) -> Optional[str]:
     username = websocket.query_params.get('username')
     if not username:
-        raise WebSocketDisconnect(code=1002)
+        await websocket.accept()  # Maybe there is better way to send user info about bad credentials
+        await websocket.send_text('You did`nt provide credentials')
+        await websocket.close()
+        raise WebSocketDisconnect(code=1)
     return username
