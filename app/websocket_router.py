ec4782f96e97d50e438aa860897ff1b261a89d14
Chenger1 <exs2199@gmail.com>
2021-09-06 22:46:41 +0300

    Create websocket endpoint

--- app/websocket_router.py
+++ app/websocket_router.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, WebSocket, FastAPI
+from fastapi import WebSocket, FastAPI, WebSocketDisconnect
+
+from connection_manager import manager
 
 
 ws = FastAPI()
@@ -10,3 +12,16 @@ async def echo_handler(websocket: WebSocket):
     while True:
         data = await websocket.receive_text()
         await websocket.send_text(f'Message: {data}')
+
+
+@ws.websocket('/{username}')
+async def websocket_endpoint(websocket: WebSocket, username: str):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f'You wrote: {data}', websocket)
+            await manager.broadcast(f'New message: {data}')
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f'Client {username} left chat')
